a0f6d780f3c297c9eb4ead24a4f3ff4f8737e563
wimbaertvlotbe <50639680+wimbaert@users.noreply.github.com>
2021-12-08 10:20:16 +0100

    Versie na einde les
    Versie na einde les. Toelichting gegeven

--- main.c
+++ main.c
@@ -13,8 +13,9 @@ int main(void)
 	volatile uint8_t waarde = 0;
 	uint8_t max = 5;
 	uint8_t reset = 3;
+	//volatile uint8_t reset = 3;
 	
-	DDRB=0x50;
+	DDRB=0x50; //0b01010000
 	volatile uint8_t ddbrstate = initDataDirectionRegister(&DDRB, 0x0F);
 	volatile uint8_t portbstate = updatePortRegister(&PORTB, 0x0A);
 
@@ -30,14 +31,14 @@ int main(void)
  *	inputs : variabele, testwaarde, resetwaarde
  *	outputs : variabele,testresultaat
  *	actie : variabele >= testwaarde? variabele=resetwaarde, testresultaat=1
- *
  */
 uint8_t testEnResetVariabele(volatile uint8_t* variabele,uint8_t testwaarde,uint8_t resetwaarde)
 {
 	uint8_t testresultaat = 0;
 	
+	//De '*' operator is de dereferencing operator
 	(*variabele)++;
-	if (*variabele >= testwaarde)
+	if (*variabele >= testwaarde )
 	{
 		*variabele=resetwaarde;
 		testresultaat = 1;		
@@ -45,14 +46,12 @@ uint8_t testEnResetVariabele(volatile uint8_t* variabele,uint8_t testwaarde,uint
 	return testresultaat;
 }
 
-
 /*
  *	Functienaam : updatePortRegister
  *
  *	inputs : pointer naar register, instelwaarde
  *	outputs : nieuwe toestand van het register
  *	actie : reg |= waarde 
- *
  */
 volatile uint8_t updatePortRegister(uint8_t* reg, uint8_t value)
 {
@@ -83,7 +82,7 @@ volatile uint8_t readPinRegister(uint8_t* reg)
  */
 volatile uint8_t initDataDirectionRegister(uint8_t* reg, uint8_t value)
 {
-	(*reg)|=value;
+	(*reg)|=value;  // *reg = *reg | value
 	 return (*reg);	
 }
 
